393d1c462760a0030751692cfb65ae1fbbe3fb93
DiegoAlbe <diegoalberghini2007@gmail.com>
2026-04-10 12:58:38 +0200

    Aggiunta carta()

--- casino-main/assets/js/script.js
+++ casino-main/assets/js/script.js
@@ -4,6 +4,10 @@
     var carte=[];//0->indice 1->numero carte 2->seme 3->se estratta  
     var giocatore = []; //carte giocatore
     var banco  = []; //carte del banco
+    var gameOver = false;
+    var saldo = 100;
+    var pg = 0; //punteggio giocatore
+    var pb = 0; //punteggio banco
 
 /* CARICAMENTO BODY */
 function avvio(){ 
@@ -15,6 +19,7 @@ function avvio(){
 function pronto(){
     controlla();//funzione che controlla il saldo
     avviaGioco(); //distribuzione iniziale delle carte sul tavolo
+    document.getElementById("pronto").style.display="none";
 
 }
 
@@ -78,8 +83,6 @@ function avviaGioco(){
                 cartaCoperta();
             }
         }
-    //mostro le carte:
-    
 }
 
 function estrazione(){
@@ -93,16 +96,43 @@ function estrazione(){
 function mostraCarta(id,isGiocatore){
     if(isGiocatore){
         idCarta=(giocatore.length-1)+"-giocatore";
+        //modifica pg
     }
     else{
         idCarta=(banco.length-1)+"-banco";
+        //modifica pb
     }
     document.getElementById(idCarta).style.width="100px";
     document.getElementById(idCarta).style.height="100px";
+    //modifico il src dell'immagine
+    nomeFile = carte[id][1];
+
+    if(carte[id][1]==1){
+        nomeFile = "ace";
+    }
+    if(carte[id][1]==11){
+        nomeFile = "jack";
+    }
+    if(carte[id][1]==12){
+        nomeFile = "queen";
+    }
+    if(carte[id][1]==13){
+        nomeFile = "king";
+    }
+
+    nomeFile = nomeFile+"_of_"+carte[id][2]+".svg";
+
+    document.getElementById(idCarta).src = "assets/img/svg-cards/"+nomeFile;
 }
 function cartaCoperta(){
     idCarta="1-banco";
     //ingrandire la carta
     document.getElementById(idCarta).style.height="100px";
     document.getElementById(idCarta).style.width="100px";
+}
+
+function carta(){
+    id = estrazione();
+    giocatore[giocatore.length] = id;
+    mostraCarta(id, true);
 }
